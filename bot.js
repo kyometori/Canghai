db0b6b0c7e56d16638219f5677b971381f45b230
wen648 <wen18842254@gmail.com>
2021-03-13 00:25:58 +0800

    embed

--- bot.js
+++ bot.js
@@ -74,6 +74,41 @@ client.on('ready', () => {
                 channel.setUserLimit(10)
             })
     })
+    command(client, 'embed', (message) => {
+        const logo =
+            'https://images-ext-1.discordapp.net/external/Rn2yYpEmFgSjGxvNOwrHO4DUr_PvOH0lVqp6QTP_qMg/https/i.imgur.com/zbXslRQ.png'
+        const embed = new Discord.MessageEmbed()
+            .setTitle('Example text embed')
+            .setURL('https://www.youtube.com/channel/UCbV4xu5u33k_fN8HPLfPnsQ')
+            .setAuthor(message.author.username)
+            .setImage(logo)
+            .setThumbnail(logo)
+            .setFooter('This is a footer')
+            .setColor('#66f5fd')
+            .setTimestamp()
+            .addFields(
+                {
+                    name: 'Field 1',
+                    value: 'Hello world',
+                    inline: true,
+                },
+                {
+                    name: 'Field 2',
+                    value: 'Hello world',
+                    inline: true,
+                },
+                {
+                    name: 'Field 3',
+                    value: 'Hello world',
+                    inline: true,
+                },
+                {
+                    name: 'Field 4',
+                    value: 'Hello world',
+                }
+            )
+        message.channel.send(embed)
+    })
     //#endregion
 })
 
