6269ed52d29b4315b8d4f073171ec7b030eb5c20
wen648 <wen18842254@gmail.com>
2021-07-09 14:24:21 +0800

    delwelcome and delleave

--- features/features/Leave.js
+++ features/features/Leave.js
@@ -1,6 +1,7 @@
 const mongo = require("@util/mongo");
 const command = require("@util/command");
 const leaveSchema = require("@schemas/Leave-schema");
+const Discord = require("discord.js");
 
 module.exports = (client) => {
   //!setleave <message>
@@ -43,6 +44,40 @@ module.exports = (client) => {
             upsert: true,
           }
         );
+        const icon = guild.iconURL();
+        const embed = new Discord.MessageEmbed()
+          .setAuthor(`${guild.name} 離開訊息設定`, `${icon}`)
+          .setColor()
+          .setFooter(`由${message.author.tag}輸入`)
+          .setTimestamp()
+          .addField("設定訊息", `${text}`)
+          .addField("發送頻道ID", `${channel.id}`, true)
+          .addField("備註", "<name> Discord 名稱\n<tag> Discord標籤", true);
+        channel.send(embed);
+      } finally {
+        mongoose.connection.close();
+      }
+    });
+  });
+
+  command(client, "delleave", async (message) => {
+    const { member, channel, guild } = message;
+    if (!member.hasPermission("ADMINISTRATOR")) {
+      channel.send("您沒有運行此命令的權限。");
+      return;
+    }
+
+    await mongo().then(async (mongoose) => {
+      try {
+        const search = await leaveSchema.findOne({ _id: `${guild.id}` });
+        //console.log(search);
+        if (search === null) {
+          channel.send("本伺服器沒設定離開訊息，使用`as@setleave`進行設定");
+          return;
+        }
+        await leaveSchema.deleteOne({ _id: `${guild.id}` });
+        channel.send("已關閉離開訊息");
+        cache[message.guild.id] = null;
       } finally {
         mongoose.connection.close();
       }
@@ -55,24 +90,28 @@ module.exports = (client) => {
     let data = cache[guild.id];
 
     if (!data) {
-      console.log("從數據庫中獲取離開數據" + guild.id);
+      console.log(`從數據庫中獲取離開數據 [${guild.id},${guild.name}]`);
 
       await mongo().then(async (mongoose) => {
         try {
           const result = await leaveSchema.findOne({ _id: guild.id });
-
+          if (!result) return;
           cache[guild.id] = data = [result.channelId, result.text];
         } finally {
           mongoose.connection.close();
         }
       });
     }
-
+    if (!data) return;
     const channelId = data[0];
     const text = data[1];
 
     const channel = guild.channels.cache.get(channelId);
-    channel.send(text.replace(/<@>/g, `${member.user.tag}`));
+    channel.send(
+      text
+        .replace(/<name>/g, `${member.user.username}`)
+        .replace(/<tag>/g, `${member.user.tag}`)
+    );
   };
 
   command(client, "simleave", (message) => {
--- features/features/welcome.js
+++ features/features/welcome.js
@@ -1,6 +1,7 @@
 const mongo = require("@util/mongo");
 const command = require("@util/command");
 const welcomeSchema = require("@schemas/welcome-schema");
+const Discord = require("discord.js");
 
 module.exports = (client) => {
   //!setwelcome <message>
@@ -43,6 +44,40 @@ module.exports = (client) => {
             upsert: true,
           }
         );
+        const icon = guild.iconURL();
+        const embed = new Discord.MessageEmbed()
+          .setAuthor(`${guild.name} 歡迎訊息設定`, `${icon}`)
+          .setColor()
+          .setFooter(`由${message.author.tag}輸入`)
+          .setTimestamp()
+          .addField("設定訊息", `${text}`)
+          .addField("發送頻道ID", `${channel.id}`, true)
+          .addField("備註", "<user> 提及該人\n<tag>  Discord標籤", true);
+        channel.send(embed);
+      } finally {
+        mongoose.connection.close();
+      }
+    });
+  });
+
+  command(client, "delwelcome", async (message) => {
+    const { member, channel, guild } = message;
+    if (!member.hasPermission("ADMINISTRATOR")) {
+      channel.send("您沒有運行此命令的權限。");
+      return;
+    }
+
+    await mongo().then(async (mongoose) => {
+      try {
+        const search = await welcomeSchema.findOne({ _id: `${guild.id}` });
+        //console.log(search);
+        if (search === null) {
+          channel.send("本伺服器沒設定歡迎訊息，使用`as@setwelcome`進行設定");
+          return;
+        }
+        await welcomeSchema.deleteOne({ _id: `${guild.id}` });
+        channel.send("已關閉歡迎訊息");
+        cache[message.guild.id] = null;
       } finally {
         mongoose.connection.close();
       }
@@ -55,24 +90,28 @@ module.exports = (client) => {
     let data = cache[guild.id];
 
     if (!data) {
-      console.log("從數據庫中獲取歡迎數據");
+      console.log(`從數據庫中獲取歡迎數據 [${guild.id},${guild.name}]`);
 
       await mongo().then(async (mongoose) => {
         try {
           const result = await welcomeSchema.findOne({ _id: guild.id });
-
+          if (!result) return;
           cache[guild.id] = data = [result.channelId, result.text];
         } finally {
           mongoose.connection.close();
         }
       });
     }
-
+    if (!data) return;
     const channelId = data[0];
     const text = data[1];
 
     const channel = guild.channels.cache.get(channelId);
-    channel.send(text.replace(/<@>/g, `<@${member.id}>`));
+    channel.send(
+      text
+        .replace(/<tag>/g, `${member.user.tag}`)
+        .replace(/<user>/g, `<@${member.id}>`)
+    );
   };
 
   command(client, "simjoin", (message) => {
